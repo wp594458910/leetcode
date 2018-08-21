2afd07fa3c6303ed7549acad1620328a05372dd3
peng <594458910@qq.com>
2018-08-21 09:26:00 +0800

    init

---
+++ src/com/leetcode/Solution0821.java
@@ -0,0 +1,21 @@
+package com.leetcode;
+
+/**
+ *  题目：从排序数组中删除重复项
+ *
+ *  给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ */
+public class Solution0821 {
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] != nums[j]){
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        j += 1;
+        return j;
+    }
+}
